5eae1363403b6b05d5f4f9989991256c77b42e1a
Manjula123-hi <danthulurimanjula@gmail.com>
2025-05-24 18:55:20 +0530

    commited

---
+++ shipping.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+dnf install maven -y
+VALIDATE $? "Installing Maven server"
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "Creating roboshop system user"
+else
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
+fi
+
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip 
+VALIDATE $? "Downloading the code in temp direcory"
+
+cd /app 
+VALIDATE $? "Moving to app directory"
+rm -rf /app/*
+
+unzip /tmp/shipping.zip
+VALIDATE $? "Unzipping shipping file"
+
+mvn clean package 
+VALIDATE $? "Installing dependencies using maven"
+
+mv target/shipping-1.0.jar shipping.jar 
+VALIDATE $? "Renaming shipping-1.0.jar with shipping.jar"
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
+VALIDATE $? "Copying shipping service to path"
+
+systemctl daemon-reload &>> $LOG_FILE
+VALIDATE $? "Reloading after changes in systemctl service file"
+
+systemctl enable shipping &>> $LOG_FILE
+VALIDATE $? "Enabling shipping service"
+
+systemctl start shipping &>> $LOG_FILE
+VALIDATE $? "Starting shipping service"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+
